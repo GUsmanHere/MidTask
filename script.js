a342b5686bc1b87ba7ca5b05ebc9b8ed90d2326f
GUsmanHere <2021bbit84@student.uet.edu.pk>
2024-04-01 22:04:24 +0500

    main

--- script.js
+++ script.js
@@ -43,15 +43,15 @@ srtop.reveal('.experience .timeline .container',{interval: 400});
 // End of Tawk.to Live Chat
 //Start of Tawk.to Script
 
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/660ada53a0c6737bd1271726/1hqd69miq';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+//var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+//(function(){
+//var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+//s1.async=true;
+//s1.src='https://embed.tawk.to/660ada53a0c6737bd1271726/1hqd69miq';
+//s1.charset='UTF-8';
+//s1.setAttribute('crossorigin','*');
+//s0.parentNode.insertBefore(s1,s0);
+//})();
 
 //End of Tawk.to Script
 
